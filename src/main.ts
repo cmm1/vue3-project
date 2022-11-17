bcbd29d66ef399b623e4e46a655df46379383ad2
zyp <qq1036641205@163.com>
2022-11-17 23:54:17 +0800

    添加pinia和router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
---
+++ src/pages/login.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+import { useUserStore } from '@/store/user'
+defineProps<{ msg: string }>()
+ const userStore = useUserStore()
+ userStore.updateName('李四')
+</script>
+<template>
+    <p>{{userStore.name}}</p>
+ </template>
+ 
