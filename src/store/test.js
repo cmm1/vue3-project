fab00cc8e1c426d8609361dd29647630a173419f
zyp <qq1036641205@163.com>
2022-12-09 00:47:20 +0800

    20221209

--- src/store/test.js
+++ src/store/test.js
@@ -1,23 +1,7 @@
 // var hello = (text: string) => {
 //     return 'hello ${text}';
 // }
-// hello('word');
-// let arrayNumbers: number[] = [1, 2, 3]
-// let arrayNumbers: [number | string, string, number] = ['1', '2', 3]
-// interface user {
-//     id: number;
-//     name: string;
-//     age: number;
-// }
-// let test: user = {
-//     id: 1,
-//     name: 'test',
-//     age: 10
-// }
-function add(x, y, z) {
-    if (typeof z === 'number') {
-        return x + y + z;
-    }
-    return x + y;
+var a = 'on';
+if (a === "on" /* enums.On */) {
+    console.log(a);
 }
-add(0, 3, '1');
