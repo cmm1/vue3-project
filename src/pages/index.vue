89477e89cb4cf72ff69f7758b52efc18f961aab0
zyp <qq1036641205@163.com>
2023-02-12 23:02:05 +0800

    20230212

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,36 @@
 
 <template>
-   <div>
-        <p>首页 {{counter}}</p> 
+   <div id="app">
+        <h1>{{counter}}</h1> 
+        <button @click="increase">+1</button>
    </div>
 </template>
 
 <script lang="ts">
-import { useInterval } from '@vueuse/core'
-import { defineComponent } from 'vue';
+import { defineComponent,ref } from 'vue';
 
      export default defineComponent({
-         name: 'VueUse',
-         setup() {
-            const { counter, pause, resume } = useInterval(200, { controls: true })
-           return {
-            counter, pause,resume
-           }
-         }
+         name: 'App',
+     //     data(){
+     //           return {
+     //                counter : 0
+     //           };
+     //     },  
+     //     methods:{
+     //      increase(){
+     //           this.counter++
+     //      }
+     //     },  
+     setup(){
+          const counter = ref(0) ;
+          const increase = ()=>{
+               counter.value ++;
+          }
+          return{
+               counter,
+               increase
+          }
+     }
      });
 
 </script>
